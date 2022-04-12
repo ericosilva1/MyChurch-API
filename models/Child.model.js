43dbd7588ba502a13fbe1e65ce376a2977498bf4
Erico Silva <tec.erico@yahoo.com.br>
2022-04-12 16:20:30 -0300

    added timestamps

--- models/Child.model.js
+++ models/Child.model.js
@@ -1,21 +1,25 @@
 const { model, Schema } = require('mongoose');
 
 const childSchema = new Schema({
-    name: {
-        type: String,
-        required: true
+        name: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        },
+        image: {
+            type: String
+        },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
     },
-    age: {
-        type: Number,
-        required: true
-    },
-    image: {
-        type: String
-    },
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+    {
+        timestamps: true
     }
-})
+)
 
 module.exports = model('Child', childSchema);
